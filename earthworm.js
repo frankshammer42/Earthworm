b974cb1c84a3d765fa0fecec404e7d1c008d0d1a
ruian2 <ruian2@illinois.edu>
2019-03-09 15:31:13 -0500

    Add quadtree

---
+++ earthworm.js
@@ -0,0 +1,107 @@
+class Earthworm {
+  constructor(x, y, ms, mf) {
+      this.position = createVector(x, y);
+      this.velocity = createVector(random(-1, 1), random(-1, 1));
+      this.acceleration = createVector(0, 0);
+      this.r = 12;
+      this.maxspeed = ms || 4;
+      this.maxforce = mf || 0.3;
+
+      //Temporary Control Parameters
+      this.desired_separation = 10;
+
+  }
+
+  applyBehaviors(earthworms) {
+      let seperateForce = this.separate(earthworms);
+      let seekForce = this.seek(earthworms);
+      // seperateForce.mult(slider1.value());
+      // seekForce.mult(slider2.value());
+      this.applyForce(seekForce);
+      this.applyForce(seperateForce);
+      this.cluster(earthworms);
+  }
+
+
+  applyForce(force){
+    this.acceleration.add(force);
+  }
+  //add separate force to objects
+  separate(earthworms){
+    let sum = createVector();
+    let count = 0;
+    for (let i = 0; i < earthworms.length; i++) {
+        let earth_worm = earthworms[i].userData;
+      let d = p5.Vector.dist(this.position, earth_worm.position);
+      if (d > 0 && d < this.desired_separation) {
+        let diff = p5.Vector.sub(this.position, earth_worm.position);
+        diff.normalize();
+        diff.div(d);
+        sum.add(diff); //sum the forces
+        count++;
+      }
+    }
+    if (count > 0) {
+      sum.div(count);
+      sum.normalize();
+      sum.mult(this.maxspeed);
+      sum.sub(this.velocity);
+      sum.limit(this.maxspeed);
+    }
+    return sum;
+  }
+
+  //TODO: compare changes to find shortest
+  seek(earthworms) {
+    let target;
+    let desiredattraction = 20;
+    for (let i = 0; i < earthworms.length; i++) {
+        let earth_worm = earthworms[i].userData;
+      let d = p5.Vector.dist(this.position, earth_worm.position);
+      if (d > 0 && d < desiredattraction) {
+        target = earth_worm.position;
+        let desired = p5.Vector.sub(target, this.position);
+        desired.normalize();
+        desired.mult(this.maxspeed);
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce);
+        return steer;
+      }
+    }
+  }
+
+  cluster(earthworms) {
+    let staydistance = 5;
+    for (let i = 0; i < earthworms.length; i++) {
+        earthworms[i] = earthworms[i].userData;
+      let d = p5.Vector.dist(this.position, earthworms[i].position);
+      if (d > 0 && d < staydistance) {
+        this.velocity = earthworms[i].velocity;
+      }
+    }
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxspeed);
+    this.acceleration.mult(0);
+  }
+
+  display() {
+    fill(255);
+    stroke(200);
+    strokeWeight(2);
+    push();
+    translate(this.position.x, this.position.y);
+    ellipse(0, 0, this.r, this.r);
+    pop();
+  }
+
+  borders() {
+    if (this.position.x > width + this.r) this.position.x = -this.r;
+    if (this.position.x < -this.r) this.position.x = width + this.r;
+    if (this.position.y > height + this.r) this.position.y = -this.r;
+    if (this.position.y < -this.r) this.position.y = height + this.r;
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,20 +1,43 @@
-let slider1, slider2, slider3;
-let vehicles = [];
+let earthworms = [];
+let total_number = 20;
+let effective_radius = 50;
+
+
+
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  slider1 = createSlider(0, 8, 4);
-  slider2 = createSlider(0, 8, 4);
-  slider3 = createSlider(0, 8, 4);
-  for (let i = 0; i < 1000; i++) {
-    vehicles.push(new Vehicle(random(width), random(height)));
-  }
+    // createCanvas(width, height);
+    createCanvas(window.innerWidth, window.innerHeight);
+    for (let i = 0; i < total_number; i++) {
+      earthworms.push(new Earthworm(random(width), random(height)));
+    }
 }
 function draw() {
-  background(51);
-  for (let v of vehicles) {
-    v.applyBehviors(vehicles);
-    v.update();
-    v.display();
-    v.borders();
-  }
+    let boundary = new Rectangle(window.innerWidth / 2, window.innerHeight / 2, window.innerWidth, window.innerHeight);
+    let qtree = new QuadTree(boundary, total_number);
+    background(51);
+
+    for (let worm of earthworms) {
+        let point = new Point(worm.position.x, worm.position.y, worm);
+        qtree.insert(point);
+    }
+
+    for (let worm of earthworms) {
+        let range = new Circle(worm.position.x, worm.position.y, effective_radius);
+        let surrounding_worms = qtree.query(range);
+        worm.applyBehaviors(surrounding_worms);
+        worm.update();
+        worm.display();
+        worm.borders();
+    }
+
+
+
+
+
+    let fps = frameRate();
+    fill(255);
+    stroke(0);
+    text("FPS: " + fps.toFixed(2), 10, height - 10);
+
+
 }
