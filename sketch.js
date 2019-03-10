3a08ef16030c68ba138a439e23274896ce9728c3
ruian2 <ruian2@illinois.edu>
2019-03-10 11:06:13 -0400

    Create another quadtree for wormgroups

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@ let ungrouped_earthworms = [];
 let worm_groups = [];
 let total_number = 100;
 let w2w_effective_radius = 200; //For Ungrouped Worm
-let worm_group_radius_threshold = 20; //If lower than this, group
+let worm_group_radius_threshold = 20; //If lower than this, group worms
+let g2g_effective_radius = 200; // For Worm Group
 //TODO: Create move effective way to filter out elements
 
 
@@ -17,17 +18,27 @@ function setup() {
 
 function draw() {
     let boundary = new Rectangle(window.innerWidth / 2, window.innerHeight / 2, window.innerWidth, window.innerHeight);
-    let qtree = new QuadTree(boundary, total_number);
+    let worm_qtree = new QuadTree(boundary, total_number);
+    let worm_group_qtree = new QuadTree(boundary, total_number);
     background(51);
 
+
+    //Insert Ungrouped Worm to its own quadtree
     for (let worm of ungrouped_earthworms) {
         let point = new Point(worm.position.x, worm.position.y, worm);
-        qtree.insert(point);
+        worm_qtree.insert(point);
     }
+
+    //Insert worm group to its own quadtree
+    for (let worm_group of worm_groups){
+        let point = new Point(worm_group.position.x, worm_group.position.y, worm_group);
+        worm_group_qtree.insert(point);
+    }
+
 
     //Create Worm Group
     for (let worm of ungrouped_earthworms) {
-        let closest = qtree.closest(new Point(worm.position.x, worm.position.y), 2, worm_group_radius_threshold);
+        let closest = worm_qtree.closest(new Point(worm.position.x, worm.position.y), 2, worm_group_radius_threshold);
         //If we have closest neighbor in radius
         if (closest.length === 2){
             let worm_0_ref = closest[0].userData;
@@ -45,15 +56,30 @@ function draw() {
         }
     }
 
-
     for (let worm of ungrouped_earthworms) {
         let range = new Circle(worm.position.x, worm.position.y, w2w_effective_radius); // let surrounding_worms = qtree.query(range);
-        let closest = qtree.closest(new Point(worm.position.x, worm.position.y), 2, w2w_effective_radius);
+        let closest = worm_qtree.closest(new Point(worm.position.x, worm.position.y), 2, w2w_effective_radius);
         worm.applyBehaviors(closest);
         worm.update();
         worm.display();
         worm.borders();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     for (let worm_group of worm_groups){
         worm_group.display();
