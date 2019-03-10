95f0dfafacbde685c1e7279ddbafafa0039ab16c
ruian2 <ruian2@illinois.edu>
2019-03-10 16:37:09 -0400

    Bug shit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 let ungrouped_earthworms = [];
 let worm_groups = [];
-let total_number = 100;
+let total_number = 50;
 let w2w_effective_radius = 200; //For Ungrouped Worm
 let worm_group_radius_threshold = 20; //If lower than this, group worms
-let g2g_effective_radius = 200; // For Worm Group
+let g2g_effective_radius = 500; // For Worm Group -> what is the range group should check
+let g2g_swallow_threshold = 30; // If below threshold, combine two groups together
 //TODO: Create move effective way to filter out elements
 
 
@@ -48,37 +49,53 @@ function draw() {
             let distance = p5.Vector.sub(worm_0_ref.position, worm_1_ref.position).mag();
             if (distance < worm_group_radius_threshold){
                 let worm_group = new Wormgroup(worm_0_clone, worm_1_clone);
-                worm_group.init();
+                worm_group.reset_worms_info();
                 worm_groups.push(worm_group);
-                ungrouped_earthworms = wormArrayRemove(ungrouped_earthworms, worm_0_ref.position);
-                ungrouped_earthworms = wormArrayRemove(ungrouped_earthworms, worm_1_ref.position);
+                ungrouped_earthworms = arrayRemove(ungrouped_earthworms, worm_0_ref.position);
+                ungrouped_earthworms = arrayRemove(ungrouped_earthworms, worm_1_ref.position);
+            }
+        }
+    }
+
+    for (let worm_group of worm_groups){
+        //Go to the group that contains the most worms
+        let range = new Circle(worm_group.position.x, worm_group.position.y, g2g_effective_radius);
+        let in_radius_wormgroups_points = worm_group_qtree.query(range);
+        //Make sure we have points
+        if (in_radius_wormgroups_points.length > 1){
+            //Sort to find the group that has the most points
+            let size = in_radius_wormgroups_points.length;
+            in_radius_wormgroups_points.sort(function (group_0, group_1) {
+                let worm_group_0 = group_0.userData;
+                let worm_group_1 = group_1.userData;
+                return worm_group_0.worms.length - worm_group_1.worms.length;
+            });
+            let group_to_apply = in_radius_wormgroups_points[size-1];
+            //Check if we should swallow the other group
+            let current_worm_group_to_apply = group_to_apply.userData;
+            let distance = p5.Vector.sub(current_worm_group_to_apply.position, worm_group.position).mag();
+            if (distance < g2g_swallow_threshold && distance !== 0){
+                worm_group.swallow(current_worm_group_to_apply);
+                // worm_group.update();
+                worm_group.borders();
+                worm_groups = arrayRemove(worm_groups, current_worm_group_to_apply.position);
+            }
+            else{
+                group_to_apply  = [group_to_apply]; //Need to pass in array
+                worm_group.applyBehaviors(group_to_apply);
+                worm_group.update();
+                worm_group.borders();
             }
         }
     }
 
     for (let worm of ungrouped_earthworms) {
-        let range = new Circle(worm.position.x, worm.position.y, w2w_effective_radius); // let surrounding_worms = qtree.query(range);
         let closest = worm_qtree.closest(new Point(worm.position.x, worm.position.y), 2, w2w_effective_radius);
         worm.applyBehaviors(closest);
         worm.update();
         worm.display();
         worm.borders();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     for (let worm_group of worm_groups){
@@ -93,7 +110,7 @@ function draw() {
 }
 
 //Utility Function
-function wormArrayRemove(arr, position) {
+function arrayRemove(arr, position) {
     return arr.filter(function(ele){
         return ele.position != position;
     });
--- wormgroup.js
+++ wormgroup.js
@@ -1,44 +1,42 @@
 class Wormgroup {
   constructor(worm_0, worm_1) {
     this.position = p5.Vector.add(worm_0.position, worm_1.position).div(2);
-    this.r = 30;
+    this.r = 50;
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
     this.worms = [worm_0, worm_1];
+    this.relative_distances = [];
     this.color = createVector(random(255), random(255), random(255));
     this.alpha = 50;
     this.earthworm_group_maxSpeed = 3;
     this.earthworm_group_maxForce = 0.1;
-    this.g2g_effective_radius = 200;
   }
 
-  init() {
+  reset_worms_info() {
+      this.relative_distances = []; //clear
     for (let worm of this.worms) {
       worm.velocity = createVector(0, 0);
       worm.acceleration = createVector(0, 0);
+      this.relative_distances.push(p5.Vector.sub(this.position, worm.position));
+      // worm.position = this.position;
     }
   }
 
   applyBehaviors(earthworms_groups) {
-    let earthworms_group_seperateForce = this.separate(earthworms_groups);
-    let earthworms_group_seekForce = this.seek(earthworms_groups);
-    // seperateForce.mult(slider1.value());
-    // seekForce.mult(slider2.value());
+    let earthworms_group_seekForce = this.seek_group(earthworms_groups);
     this.applyForce(earthworms_group_seekForce);
-    this.applyForce(earthworms_group_seperateForce);
-    // this.cluster(ungrouped_earthworms);
   }
 
   applyForce(force) {
     this.acceleration.add(force);
   }
 
-  seek(earthworms_groups) {
+  seek_group(earthworms_groups) {
     let target;
     for (let i = 0; i < earthworms_groups.length; i++) {
       let earth_worm_group = earthworms_groups[i].userData;
       let d = p5.Vector.dist(this.position, earth_worm_group.position);
-      if (d > 0 && d < this.g2g_effective_radius) {
+      if (d > 0) {
         target = earth_worm_group.position;
         let desired = p5.Vector.sub(target, this.position);
         desired.normalize();
@@ -49,15 +47,20 @@ class Wormgroup {
       }
     }
   }
+
   update() {
-    this.position.add(this.velocity);
-    this.velocity.add(this.acceleration);
-    this.acceleration.mult(0);
-    this.velocity.limit(this.maxSpeed);
+      this.position.add(this.velocity);
+      this.velocity.add(this.acceleration);
+      this.acceleration.mult(0);
+      this.velocity.limit(this.earthworm_group_maxSpeed);
+      for (let i=0; i<this.worms.length; i++){
+          this.worms[i].position = p5.Vector.add(this.relative_distances[i], this.position);
+      }
   }
+
   display() {
     for (let worm of this.worms) {
-      worm.display();
+        worm.display();
     }
     fill(this.color.x, this.color.y, this.color.z, this.alpha);
     stroke(200);
@@ -67,4 +70,20 @@ class Wormgroup {
     ellipse(0, 0, this.r, this.r);
     pop();
   }
+
+    borders() {
+        if (this.position.x > width + this.r) this.position.x = -this.r;
+        if (this.position.x < -this.r) this.position.x = width + this.r;
+        if (this.position.y > height + this.r) this.position.y = -this.r;
+        if (this.position.y < -this.r) this.position.y = height + this.r;
+    }
+
+    swallow(other_group){
+      for (let worm of other_group.worms){
+          this.worms.push(worm);
+      }
+      this.reset_worms_info();
+    }
+
+
 }
